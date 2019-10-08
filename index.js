2f9ac1f551709942a8035ffc72ad53d5811d9722
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-10-08 13:42:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Write your solution in this file!
+const driver = {car:"mercedes",year:"2019"};
+
 function updateDriverWithKeyAndValue(driver,key,value){
   return Object.assign({},driver,{key:value});
 }
