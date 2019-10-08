40f97f0f58d2c6ec2d2fa44f53712c3959d6437d
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-10-08 13:43:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const driver = {
 };
 
 function updateDriverWithKeyAndValue(driver,key,value){
-  return Object.assign({},driver,{key:value});
+  return Object.assign({},driver,{[key]:value});
 }
 function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
-  return Object.assign(driver,{key:value});
+  return Object.assign(driver,{[key]:value});
 }
 function deleteFromDriverByKey(driver,key){
   const obj = Object.assign({},driver);
